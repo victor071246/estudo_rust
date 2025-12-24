0c98c8508d57f3a815825b59cd5d14ffc9905d13
v <daderiovictorfelipe@gmail.com>
2025-12-23 23:44:54 -0300

    remove target do versionamento

--- tarefas_2/padrao_de_palavras_rust/target/debug/deps/padrao_de_palavras_rust-c5279c6355610bae.d
+++
@@ -1,5 +0,0 @@
-/home/v/estudo_rust/tarefas_2/padrao_de_palavras_rust/target/debug/deps/padrao_de_palavras_rust-c5279c6355610bae.d: src/main.rs
-
-/home/v/estudo_rust/tarefas_2/padrao_de_palavras_rust/target/debug/deps/libpadrao_de_palavras_rust-c5279c6355610bae.rmeta: src/main.rs
-
-src/main.rs:

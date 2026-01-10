3bd49b3c1edafd2ee96d8f01571160920083702f
v <daderiovictorfelipe@gmail.com>
2026-01-10 17:01:05 -0300

    Escrevendo arquivos em rust

---
+++ aprofundando/escrevendo_arquivos/src/main.rs
@@ -0,0 +1,7 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main(){
+    let mut arquivo = File::create("teste.txt").expect("Erro ao criar o arquivo");
+    arquivo.write_all(b"Arquivo de teste sendo criado").expect("Não conseguiu escrever no arquivo");
+}

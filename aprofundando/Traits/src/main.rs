637faf72eb62efb08f41ab37c7c435495ef3452b
v <daderiovictorfelipe@gmail.com>
2026-01-10 17:12:20 -0300

    traits em Rust, algo como uma implementação contratual ou algo assim

---
+++ aprofundando/Traits/src/main.rs
@@ -0,0 +1,32 @@
+struct Pessoa{
+    nome: String,
+    idade: i32
+}
+
+trait Voz{
+    fn falar(&self);
+
+    fn tem_voz(&self) -> bool;
+}
+
+impl Voz for Pessoa{
+
+    fn falar(&self){
+        println!("Olá, meu nome é {}", self.nome);
+    }
+
+    fn tem_voz(&self) -> bool {
+        if self.idade > 1 {
+            return true;
+        }
+        return false
+    }
+}
+fn main(){
+    let pessoa = Pessoa{
+        nome: String::from("João"),
+        idade: 42
+    };
+
+    println!("Pode {} falar ? {}", pessoa.nome, pessoa.tem_voz())
+}

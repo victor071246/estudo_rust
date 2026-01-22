706191b9c6f9ee537a4931525de326c3cf073351
v <daderiovictorfelipe@gmail.com>
2026-01-21 23:47:32 -0300

    exercicio de mediana, os dois são diferentes apesar de estarem com o mesmo nome

--- projetos_curso/mediana_vetor/src/main.rs
+++ projetos_curso/mediana_vetor/src/main.rs
@@ -1,26 +1,44 @@
-fn media(numeros: &Vec<i64>) -> f64 {
-
-    let mut soma = 0;
-    for i in numeros {
-        soma += 1;
-    }
-
-    soma as f64 / numeros.len() as f64
+fn media(numeros: &Vec<f64>) -> f64 {
+    let sum: f64 = numeros.iter().sum();
+    sum / numeros.len() as f64
 }
 
-fn mediana_vetor(vetor: &Vec<i64>) -> f64 {
-    let mut numeros_sorted = vetor.clone();
-    numeros_sorted.sort();
-
-    let numero_meio = vetor.len() / 2;
-    if numeros_sorted.len() % 2 == 0 {
-        return media( &vec![numeros_sorted[numero_meio], numeros_sorted[numero_meio -1]]);
-        
+fn mediana(vetor: &Vec<i64>) -> Result<f64, &'static str> {
+    if vetor.is_empty(){
+        return Err("O vetor está vazio");
     }
 
-    numeros_sorted[numero_meio] as f64
+    let mut sorted_numeros = vetor.clone();
+    sorted_numeros.sort();
+    let len = sorted_numeros.len();
+    if len % 2 == 1{
+        Ok(sorted_numeros[len/2] as  f64)
+    } else {
+        let mid1 = sorted_numeros[len/2 - 1] as f64;
+        let mid2 = sorted_numeros[len/2] as f64;
+        Ok(media(&vec![mid1, mid2]))
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let numeros_impares = vec![3, 1, 2]; //mediana: 2
+    let numeros_pares = vec![4, 1, 3, 2]; // mediana: 2.5
+    let numeros_vazio:Vec<i64> = vec![];
+
+    match mediana(&numeros_impares){
+        Ok(mediana) => println!("A mediana dos números ímpares é: {}", mediana),
+        Err(e) => println!("Erro: {}", e)
+    }
+
+    match mediana(&numeros_pares){
+        Ok(mediana) => println!("A mediana dos números ímpares é: {}", mediana),
+        Err(e) => println!("Erro: {}", e)
+    }
+
+    match mediana(&numeros_vazio){
+        Ok(mediana) => println!("A mediana dos números ímpares é: {}", mediana),
+        Err(e) => println!("Erro: {}", e)
+    }
+
+
 }

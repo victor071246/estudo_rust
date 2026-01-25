e1234faabf862c19c5295d32c6e9a0385d601bc7
v <daderiovictorfelipe@gmail.com>
2026-01-24 23:33:28 -0300

    finalizando com leitura de arquivo

--- projetos_curso/estatisticas_descritivas/src/main.rs
+++ projetos_curso/estatisticas_descritivas/src/main.rs
@@ -1,5 +1,26 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 use estatisticas_descritivas::executar_estatisticas_descritivas;
+
+fn ler_numeros(nome_arquivo: &str) -> Result<Vec<i64>, std::io::Error>{
+    let arquivo = File::open(nome_arquivo)?;
+    let leitor = BufReader::new(arquivo);
+    let mut numeros = vec![];
+
+    for linha in leitor.lines(){
+        let linha = linha?;
+        if let Ok(linha) = linha.trim().parse::<i64>(){
+            numeros.push(linha);
+        }
+    }
+
+    Ok(numeros)
+}
 fn main() {
-    let numeros: Vec<i64> = vec![1, 1, 2, 3, 4, 4, 4, 5, 6, 0];
+    let numeros: Vec<i64> = ler_numeros("dados.txt").unwrap_or_else(|err| {
+        eprintln!("Erro ao ler o arquivo: {}", err);
+        std::process::exit(1);
+    });
     executar_estatisticas_descritivas(numeros);
 }

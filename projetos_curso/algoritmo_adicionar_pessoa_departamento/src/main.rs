a2f5e2919e3a84b848e180584b08461880710108
v <daderiovictorfelipe@gmail.com>
2026-01-25 23:32:43 -0300

    Programa simples para adicionar pessoa em um departamento, um começo pra outros projetos

--- projetos_curso/algoritmo_adicionar_pessoa_departamento/src/main.rs
+++ projetos_curso/algoritmo_adicionar_pessoa_departamento/src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, io};
 
 fn departamento(){
-    //let mut departamento_pessoas = HashMap::new();
+    let mut departamento_pessoas = HashMap::new();
 
     loop{
         println!("Digite o comando do tipo: 'Adicionar <Pessoa> para <Departamento>");
@@ -10,15 +10,29 @@ fn departamento(){
 
         let comando = comando.trim();
         let mut token_comando = comando.split_whitespace();
-        println!("{:?}", token_comando.nth(1));
+        //println!("{:?}", token_comando.nth(1));
+        //println!("{:?}", token_comando.nth(1));
 
         let pessoa = match token_comando.nth(1){
             Some(p) => p,
             None => {
-                println!("O comando digitado não é valido");
+                println!("1. O comando digitado não é valido");
                 continue;
             }
         };
+
+        let departamento = match token_comando.nth(1){
+            Some(d) => d,
+            None => {
+                println!("2. O comando digitado não é válido");
+                continue;
+            }
+        };
+
+        let empregado = departamento_pessoas.entry(String::from(departamento)).or_insert(vec![]);
+        empregado.push(String::from(pessoa));
+        println!("{:?}", departamento_pessoas);
+
     }
 }
 
